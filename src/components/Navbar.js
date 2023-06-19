e76c6a02bb5524afc3c122cf2d5e3c1c97f0fd9d
Mrs_N <worksmart377@gmail.com>
2023-06-18 20:36:59 -0500

    adds css and edits navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,85 @@
-import React from 'react'
-import Logo from '../assets/SN-logo.jpg'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import $ from 'jquery'
+import React from 'react';
+import Logo from '../assets/SN-logo.jpg';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import $ from 'jquery';
+import { useState } from 'react';
 
 const Navbar = () => {
-    return (
+    const [nav, setNav] = useState(false)
+    const handleClick= ()=> setNav(!nav)
+  return (
     <div>
-        <nav class="navbar navbar-expand-lg" data-bs-theme="dark" style={{height: '150px'}}>
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src={Logo} alt="SN Logo" style={{width: '150px'}} /></a>
-            
-            <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                <a class="nav-link active" href="#">Home
-                    <span class="visually-hidden">(current)</span>
+      <nav
+        class='navbar navbar-expand-lg'
+        data-bs-theme='dark'
+        style={{ height: '150px' }}
+      >
+        <div class='container-fluid'>
+          <a class='navbar-brand' href='#'>
+            <img src={Logo} alt='SN Logo' style={{ width: '150px' }} />
+          </a>
+
+          <div class='collapse navbar-collapse' id='navbarColor02'>
+            <ul class='navbar-nav me-auto'>
+              <li class='nav-item'>
+                <a class='nav-link active' href='#'>
+                  Home
+                  <span class='visually-hidden'>(current)</span>
                 </a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Skills</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My Work</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Programming</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Art & Design</a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
+                  About
+                </a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
+                  Skills
+                </a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
+                  Contact
+                </a>
+              </li>
+              <li class='nav-item dropdown'>
+                <a
+                  class='nav-link dropdown-toggle'
+                  data-bs-toggle='dropdown'
+                  href='#'
+                  role='button'
+                  aria-haspopup='true'
+                  aria-expanded='false'
+                >
+                  My Work
+                </a>
+                <div class='dropdown-menu'>
+                  <a class='dropdown-item' href='#'>
+                    Programming
+                  </a>
+                  <div class='dropdown-divider'></div>
+                  <a class='dropdown-item' href='#'>
+                    Art & Design
+                  </a>
                 </div>
-                </li>
-                </ul>
-            </div>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
+        <div onclick={handleClick} className='container-sm'>
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+        <ul className={!nav ? '.d-none' : 'container-sm' }>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>My Programming Work</li>
+          <li>My Art & Design Work</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-</div>
-    )
-}
-
-export default Navbar
+export default Navbar;
