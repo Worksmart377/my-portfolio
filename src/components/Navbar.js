67177758722ff0d218aeb9b1f5e602e1cbf9b17b
Mrs_N <worksmart377@gmail.com>
2023-06-20 21:45:49 -0500

    adds contact dropdown with socials in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,39 @@ import React from 'react';
 import Logo from '../assets/SN-logo.jpg';
 import {Navbar, Nav, NavDropdown, NavLink } from 'react-bootstrap';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import { HiOutlineMail, HiOutlineDocumentText } from 'react-icons/hi';
+import { FaGithub, FaLinkedin, FaBehanceSquare } from 'react-icons/fa';
+
 
 const MyNavbar = () => {
     return (
         <div>
             <Navbar sticky='top' expand='sm' collapseOnSelect>
-                <nav class='navbar navbar-expand-lg'data-bs-theme='light'style={{ height: '150px' }}>        
+                <nav class='navbar navbar-expand-lg navbar-light bg-transparent' style={{ height: '150px' }}>        
                     <div class='container-fluid'>
-
                         <Navbar.Brand>
                             <a class='navbar-brand' href='About'>
                             <img src={Logo} alt='SN Logo' style={{ width: '150px' }} />
                             </a>           
                         </Navbar.Brand>
-                        <Navbar.Toggle/>
+                        <NavbarToggle/>
                         <Navbar.Collapse>
                         <Nav>
                         
                             <NavLink href='About'>About</NavLink>
                             <NavLink href='Skills'>Skills</NavLink>
-                            <NavLink href='Contact'>Contact</NavLink>
-                            <NavDropdown title='My Work'>
+                            <NavDropdown title='Contact'>
+                                <NavDropdown.Item href='https://www.linkedin.com/in/stephany-nelson'><FaLinkedin size={40} /></NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href='https://github.com/Worksmart377'><FaGithub size={40} /></NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href='https://www.behance.net/MrsN377'><FaBehanceSquare size={40} /></NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href='Email'><HiOutlineMail size={40} /></NavDropdown.Item>                                
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href='Resume'><HiOutlineDocumentText size={40} /></NavDropdown.Item>
+                                </NavDropdown>                            
+                                <NavDropdown title='My Work'>
                                 <NavDropdown.Item href='Programming'>Programming</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href='Art & Design'>Art & Design</NavDropdown.Item>
